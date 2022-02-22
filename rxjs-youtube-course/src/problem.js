c8284e760dc7fa7c65fe21bb8525f73673602ac3
Serhii Kredentser <sergeykredenser@gmail.com>
2022-02-22 09:09:44 +0200

    New project

---
+++ rxjs-youtube-course/src/problem.js
@@ -0,0 +1,45 @@
+import { interval } from "rxjs";
+import { filter, map, take } from "rxjs/operators";
+
+
+const btn = document.getElementById('interval')
+const rxjsBtn = document.getElementById('rxjs')
+const display = document.querySelector('#problem .result')
+
+const people = [
+    { name: 'Vladilen', age: 25 },
+    { name: 'Elena', age: 17 },
+    { name: 'Ivan', age: 18 },
+    { name: 'Igor', age: 14 },
+    { name: 'Lisa', age: 32 },
+    { name: 'Irina', age: 23 },
+    { name: 'Oleg', age: 20 }
+]
+
+btn.addEventListener('click', () => {
+    btn.disabled = true;
+    let i = 0;
+    let canDrink = [];
+    const interval = setInterval(() => {
+        if (people[i]) {
+            if (people[i].age >= 18) {
+                canDrink.push(people[i].name);
+            }
+            display.textContent = canDrink.join(' ');
+            i++;
+        } else {
+            clearInterval(interval);
+            btn.disabled = false;
+        }
+    }, 1000);
+})
+
+rxjsBtn.addEventListener('click', () => {
+    interval(1000).pipe(
+        take(people.length),
+        filter(value => people[value] >= 18),
+        map(i => people[i].name)
+    ).subscribe(value =>
+        display.textContent = value
+    );
+})
